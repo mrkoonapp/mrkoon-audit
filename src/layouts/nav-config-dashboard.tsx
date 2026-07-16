2a740758fbd80ec51c19872ea566914e23dd607d
HusseinSalah96 <hussein_salah@mrkoonapp.com>
2026-07-16 17:17:56 +0300

    comment all except home

--- src/layouts/nav-config-dashboard.tsx
+++ src/layouts/nav-config-dashboard.tsx
@@ -37,10 +37,10 @@ export function navData(t: TFunction<any, any>): NavSectionProps['data'] {
       subheader: t('overview'),
       items: [
         { title: t('dashboard'), path: paths.dashboard.root, icon: ICONS.dashboard },
-        { title: t('auctions'), path: paths.dashboard.auctions, icon: ICONS.auctions },
-        { title: t('inspections'), path: paths.dashboard.inspections, icon: ICONS.inspections },
-        { title: t('sales'), path: paths.dashboard.sales, icon: ICONS.sales },
-        { title: t('operations'), path: paths.dashboard.operations, icon: ICONS.operations },
+        // { title: t('auctions'), path: paths.dashboard.auctions, icon: ICONS.auctions },
+        // { title: t('inspections'), path: paths.dashboard.inspections, icon: ICONS.inspections },
+        // { title: t('sales'), path: paths.dashboard.sales, icon: ICONS.sales },
+        // { title: t('operations'), path: paths.dashboard.operations, icon: ICONS.operations },
       ],
     },
   ];
